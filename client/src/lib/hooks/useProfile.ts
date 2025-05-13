30175ae85bbb7ada19999932f9214aabc0d65302
vedang singh <vedang012@gmail.com>
2025-05-14 02:32:29 +0530

    End of section 18

--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import agent from "../api/agent";
 import { useMemo } from "react";
+import { EditProfileSchema } from "../schemas/editProfileSchema";
 
 export const useProfile = (id?: string) =>
 {
@@ -110,6 +111,30 @@ export const useProfile = (id?: string) =>
         return id == queryClient.getQueryData<User>(['user'])?.id;
     }, [id, queryClient]);
 
+
+    const updateProfile = useMutation({
+        mutationFn: async (profile: EditProfileSchema) => {
+            await agent.put(`/profiles`, profile);
+        },
+        onSuccess: (_, profile) => {
+            queryClient.setQueryData(['profile', id], (data: Profile) => {
+                if (!data) return data;
+                return {
+                    ...data,
+                    displayName: profile.displayName,
+                    bio: profile.bio
+                }
+            });
+            queryClient.setQueryData(['user'], (userData: User) => {
+                if (!userData) return userData;
+                return {
+                    ...userData,
+                    displayName: profile.displayName
+                }
+            });
+        }
+    })
+
     return {
         profile,
         loadingProfile,
@@ -118,6 +143,7 @@ export const useProfile = (id?: string) =>
         isCurrentUser,
         uploadPhoto,
         setMainPhoto,
-        deletePhoto
+        deletePhoto,
+        updateProfile
     };
 }
